ca7982f0748ddf4403d6314756ba94e52b6e40a1
iankigen <ian.gabe.ian@gmail.com>
2017-11-21 15:20:24 +0300

    [chore] add app config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Defining constants
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_NAME', 'shareboard');
+
+// Define URL
+define('ROOT_PATH', '/');
+define('ROOT_URL', 'http://localhost/shareboard-MVC/');
+
