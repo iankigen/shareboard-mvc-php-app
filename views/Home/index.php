7c13eb88a214dd552663b380e18e609275bab19d
iankigen <ian.gabe.ian@gmail.com>
2017-11-21 15:19:25 +0300

    [chore] add views

---
+++ views/Home/index.php
@@ -0,0 +1,8 @@
+<div class="text-center">
+    <h1>Welcome to Shareboard</h1>
+
+    <p class="lead">
+        Find something cool? Share it with with our community
+    </p>
+    <a href="<?php echo ROOT_URL;?>shares" class="btn btn-primary text-center">Share Now</a>
+</div>
---
+++ views/Shares/add.php
@@ -0,0 +1,23 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">Share something</h3>
+    </div>
+    <div class="panel-body">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+            <div class="form-group">
+                <label for="">Share Title</label>
+                <input type="text" name="title" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Body</label>
+                <textarea name="body" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="">Link</label>
+                <input type="text" name="link" class="form-control">
+            </div>
+            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+            <a href="<?php echo ROOT_URL;?>shares" class="btn btn-danger">Cancel</a>
+        </form>
+    </div>
+</div>
